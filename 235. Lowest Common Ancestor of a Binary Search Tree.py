e497563b4852083b828c3102528080a79409053e
jhuang30 <jieyinghuang30@gmail.com>
2021-03-02 20:07:30 -0500

    236

--- 235. Lowest Common Ancestor of a Binary Search Tree.py
+++ 235. Lowest Common Ancestor of a Binary Search Tree.py
@@ -2,7 +2,7 @@
 
 # According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between 
 # two nodes p and q as the lowest node in T that has both p and q as descendants 
-(where we allow a node to be a descendant of itself).”
+# (where we allow a node to be a descendant of itself).”
 
  
 
