f13b099244077a4e0a7c0f9039007d72ab89d33e
jhuang30 <jieyinghuang30@gmail.com>
2021-06-05 15:05:16 -0400

    589

--- 589. N-ary Tree Preorder Traversal.py
+++ 589. N-ary Tree Preorder Traversal.py
@@ -2,10 +2,8 @@
 
 # Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 
- 
 
 # Example 1:
-
 
 
 # Input: root = [1,null,3,2,4,null,5,6]
@@ -13,10 +11,9 @@
 # Example 2:
 
 
-
 # Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
 # Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
- 
+
 
 # Constraints:
 
@@ -24,6 +21,7 @@
 # 0 <= Node.val <= 104
 # The height of the n-ary tree is less than or equal to 1000.
 
+# dfs
 class Solution:
     def preorder(self, root: 'Node') -> List[int]:
         self.res = []
@@ -37,3 +35,18 @@ class Solution:
 
         dfs(root)
         return self.res
+
+# bfs
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        res = []
+        stack = [root]
+        while stack:
+            current = stack.pop()
+            if current:
+                res.append(current.val)
+                for child in current.children[::-1]:
+                    stack.append(child)
+        return res
