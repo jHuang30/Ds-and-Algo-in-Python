e3016ef9dcf912f7cc402fcbe0086709dfb65786
jhuang30 <jieyinghuang30@gmail.com>
2021-06-12 17:24:33 -0400

    101

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -38,3 +38,16 @@ class Solution:
             queue.append(node1.right)
             queue.append(node2.left)
         return True
+
+# dfs:
+
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        def dfs(root1, root2):
+            if not root1 and not root2:
+                return True
+            if not root1 or not root2:
+                return False
+            return root1.val == root2.val and dfs(root1.left, root2.right) and dfs(root1.right, root2.left)
+        return dfs(root, root)
