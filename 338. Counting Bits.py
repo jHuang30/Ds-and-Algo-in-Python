469ae69fe21532b1be308d5c4d2b5860f7931590
jhuang30 <jieyinghuang30@gmail.com>
2021-06-19 10:20:51 -0400

    338

--- 338. Counting Bits.py
+++ 338. Counting Bits.py
@@ -32,3 +32,11 @@ class Solution:
         for i in range(1, num + 1):
             res[i] = res[i >> 1] + (i & 1)
         return res
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        dp = [0]
+        for i in range(1, n + 1):
+            dp.append(dp[i >> 1] + (i & 1))
+        return dp
