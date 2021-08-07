518b2d71877e2a736a422a6665c78ee313bc60f1
jhuang30 <jieyinghuang30@gmail.com>
2021-08-06 21:48:59 -0400

    221

--- 221. Maximal Square.py
+++ 221. Maximal Square.py
@@ -42,3 +42,18 @@ class Solution:
                 max_len = max(max_len, dp[j])
                 prev = temp
         return max_len * max_len
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        dp = [[0] * len(matrix[0]) for _ in range(len(matrix))]
+        res = 0
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if i == 0 or j == 0 or matrix[i][j] == "0":
+                    dp[i][j] = int(matrix[i][j])
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
+                res = max(res, dp[i][j] * dp[i][j])
+                    
+        return res
