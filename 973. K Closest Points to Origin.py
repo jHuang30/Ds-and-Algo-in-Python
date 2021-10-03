a0189cdd36e9c48c007f11f32c3c0c06fa8cb8d4
jhuang30 <jieyinghuang30@gmail.com>
2021-10-03 16:19:06 -0400

    973

--- 973. K Closest Points to Origin.py
+++ 973. K Closest Points to Origin.py
@@ -62,5 +62,6 @@ class Solution:
 
 
 # heap
-
-  
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return heapq.nsmallest(k, points, key = lambda point: point[0]**2 + point[1]**2)
